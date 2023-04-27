7cdfd3435db5c58082f4d53ea6a8ec8c778b7ecb
Leonardo Staniecki Angonese <koala.leo17@gmail.com>
2023-04-27 17:17:50 -0300

    initial commit

---
+++ js/core.js
@@ -0,0 +1,24 @@
+const carouselItems = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
+
+function changeSlide() {
+  // oculta o slide atual
+  carouselItems[currentIndex].classList.remove('active');
+
+  // incrementa o índice do slide atual
+  currentIndex++;
+
+  // verifica se o índice é maior do que o número de slides
+  if (currentIndex >= carouselItems.length) {
+    currentIndex = 0;
+  }
+
+  // exibe o próximo slide
+  carouselItems[currentIndex].classList.add('active');
+}
+
+// exibe o primeiro slide
+carouselItems[currentIndex].classList.add('active');
+
+// alterna os slides a cada 5 segundos
+setInterval(changeSlide, 5000);
