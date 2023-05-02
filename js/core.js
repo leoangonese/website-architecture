f26b1fae3d8bdaaf7030ad0f8d38b348861ced0f
Leonardo Staniecki Angonese <koala.leo17@gmail.com>
2023-05-02 14:13:21 -0300

    adjusted items

--- js/core.js
+++ js/core.js
@@ -1,24 +1,33 @@
-const carouselItems = document.querySelectorAll('.carousel-item');
-let currentIndex = 0;
+const carouselItems = document.querySelectorAll('.carousel-item')
+let currentIndex = 0
 
 function changeSlide() {
   // oculta o slide atual
-  carouselItems[currentIndex].classList.remove('active');
+  carouselItems[currentIndex].classList.remove('active')
 
   // incrementa o índice do slide atual
-  currentIndex++;
+  currentIndex++
 
   // verifica se o índice é maior do que o número de slides
   if (currentIndex >= carouselItems.length) {
-    currentIndex = 0;
+    currentIndex = 0
   }
 
   // exibe o próximo slide
-  carouselItems[currentIndex].classList.add('active');
+  carouselItems[currentIndex].classList.add('active')
 }
 
 // exibe o primeiro slide
-carouselItems[currentIndex].classList.add('active');
+carouselItems[currentIndex].classList.add('active')
 
 // alterna os slides a cada 5 segundos
-setInterval(changeSlide, 5000);
+setInterval(changeSlide, 5000)
+
+const questions = document.querySelectorAll('.question')
+
+questions.forEach((question) => {
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling
+    answer.classList.toggle('show')
+  })
+})
