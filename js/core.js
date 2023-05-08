b4a6268e8d1bef9b84f26dc8c82d4a1a13484d8b
Leonardo Staniecki Angonese <koala.leo17@gmail.com>
2023-05-08 17:07:19 -0300

    adjutedForm

--- js/core.js
+++ js/core.js
@@ -31,3 +31,57 @@ questions.forEach((question) => {
     answer.classList.toggle('show')
   })
 })
+
+// Contato
+var nome = document.querySelector('#nome')
+var assunto = document.querySelector('#assunto')
+var email = document.querySelector('#email')
+var celular = document.querySelector('#celular')
+var descricao = document.querySelector('#descricao')
+
+// RegEx
+const nameRegex = /[A-Z][a-z]* [A-Z][a-z]*/
+const emailRegex = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g
+
+// error
+
+const SendForm = () => {
+  if (!nameRegex.test(nome.value)) {
+    document.getElementById('error-nome').style.display = 'block'
+    document.getElementById('nome').focus()
+  } else {
+    document.getElementById('error-nome').style.display = 'none'
+    document.getElementById('assunto').focus()
+    if (assunto.value === '') {
+      document.getElementById('error-assunto').style.display = 'block'
+      document.getElementById('assunto').focus()
+    } else {
+      document.getElementById('error-assunto').style.display = 'none'
+      document.getElementById('email').focus()
+      if (!emailRegex.test(email.value)) {
+        document.getElementById('error-email').style.display = 'block'
+        document.getElementById('email').focus()
+      } else {
+        document.getElementById('error-email').style.display = 'none'
+        document.getElementById('celular').focus()
+        if (celular.value.length < 8) {
+          document.getElementById('error-celular').style.display = 'block'
+          document.getElementById('celular').focus()
+        } else {
+          document.getElementById('error-celular').style.display = 'none'
+          document.getElementById('descricao').focus()
+          if (descricao.value.length < 15) {
+            document.getElementById('error-descricao').style.display = 'block'
+            document.getElementById('descricao').focus()
+          } else {
+            document.getElementById('error-descricao').style.display = 'none'
+            window.open(
+              `https://wa.me/5551991697961?text=Nome:++${nome.value}%0D%0DE-mail:++${assunto.value}%0D%0DE-mail:++${email.value}%0D%0DCelular:++${celular.value}%0D%0DProjeto:++${descricao.value}`
+            )
+            window.alert('Enviado com sucesso')
+          }
+        }
+      }
+    }
+  }
+}
